94538fffeea414a0513db2c1fd9094373fa4b257
abhiiiiiiijit <abhijitjan22@gmail.com>
2024-10-23 00:52:58 +0200

    Pyspark installed

--- example.py
+++ example.py
@@ -1,7 +1,18 @@
-a = 3
-b = 3
+# Imports
+from pyspark.sql import SparkSession
 
-if a==b:
-    print("Same")
-else:
-    print("different")
+# Create SparkSession
+spark = SparkSession.builder.appName('PySpark Sample DataFrame').getOrCreate()
+
+# Define Schema
+col_schema = ["Language", "Version"]
+
+# Prepare Data
+Data = (("Jdk","17.0.12"), ("Python", "3.11.9"), ("Spark", "3.5.1"),   \
+    ("Hadoop", "3.3 and later"), ("Winutils", "3.6"),  \
+  )
+
+# Create DataFrame
+df = spark.createDataFrame(data = Data, schema = col_schema)
+df.printSchema()
+df.show(5,truncate=False)
